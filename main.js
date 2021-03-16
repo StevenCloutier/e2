0c814ded152cbd6c573a3ab6c131e487b99b5b24
steve <lunar75star@gmail.com>
2021-03-16 12:20:47 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+alert ('hello world');
+var name= prompt('what is your frist name');var name =prompt('what is your last name ');
+var age =18;
+if (age <18){
+alert ("you can not enter")
+};
+else if (age <18) {
+	alert ("you can enter")
+};
+alert ("hey there");
